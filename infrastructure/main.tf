f5eeb061004a514c0727816aab145b49dae15ecf
Matt Hoffmeister <matt.hoffmeister@gmail.com>
2025-05-08 15:25:14 -0500

    Refactor formatting in main.tf for improved readability

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -25,7 +25,7 @@ provider "azurerm" {
     }
   }
 
-  use_oidc = true
+  use_oidc        = true
   subscription_id = var.subscription_id
 }
 
@@ -48,9 +48,9 @@ module "law" {
   source  = "Azure/avm-res-operationalinsights-workspace/azurerm"
   version = "0.4.2"
 
-  name                                      = module.naming.log_analytics_workspace.name
-  location                                  = var.location
-  resource_group_name                       = azurerm_resource_group.this.name
+  name                = module.naming.log_analytics_workspace.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
   log_analytics_workspace_identity = {
     type = "SystemAssigned"
   }
@@ -58,72 +58,72 @@ module "law" {
 
 # Application Insights for the Psi API
 module "appi_psi" {
-    source  = "Azure/avm-res-insights-component/azurerm"
-    version = "0.1.5"
+  source  = "Azure/avm-res-insights-component/azurerm"
+  version = "0.1.5"
 
-    name                = join("-", ["appi", "appidemo", "psi", var.location])
-    location            = var.location
-    resource_group_name = azurerm_resource_group.this.name
-    workspace_id        = module.law.resource_id
+  name                = join("-", ["appi", "appidemo", "psi", var.location])
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  workspace_id        = module.law.resource_id
 }
 
 # Application Insights for the Omega API
 module "appi_omega" {
-    source  = "Azure/avm-res-insights-component/azurerm"
-    version = "0.1.5"
+  source  = "Azure/avm-res-insights-component/azurerm"
+  version = "0.1.5"
 
-    name                = join("-", ["appi", "appidemo", "omega", var.location])
-    location            = var.location
-    resource_group_name = azurerm_resource_group.this.name
-    workspace_id        = module.law.resource_id
+  name                = join("-", ["appi", "appidemo", "omega", var.location])
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  workspace_id        = module.law.resource_id
 }
 
 # App Service Plan for the APIs
 module "asp" {
-    source  = "Azure/avm-res-web-serverfarm/azurerm"
-    version = "0.5.0"
+  source  = "Azure/avm-res-web-serverfarm/azurerm"
+  version = "0.5.0"
 
-    name                = module.naming.app_service_plan.name
-    location            = var.location
-    resource_group_name = azurerm_resource_group.this.name
-    os_type = "Linux"
-    sku_name = "B1"
+  name                = module.naming.app_service_plan.name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
 # App Service for the Psi API
 module "app_psi" {
-    source  = "Azure/avm-res-web-site/azurerm"
-    version = "0.16.0"
+  source  = "Azure/avm-res-web-site/azurerm"
+  version = "0.16.0"
 
-    name                = join("-", ["app", "appidemo", "psi", var.location])
-    location            = var.location
-    resource_group_name = azurerm_resource_group.this.name
-    kind = "webapp"
+  name                = join("-", ["app", "appidemo", "psi", var.location])
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  kind                = "webapp"
 
-    os_type                  = module.asp.resource.os_type
-    service_plan_resource_id = module.asp.resource_id
+  os_type                  = module.asp.resource.os_type
+  service_plan_resource_id = module.asp.resource_id
 
-    application_insights = {
-        name = join("-", ["appi", "appidemo", "psi", var.location])
-        workspace_resource_id = module.law.resource_id
-    }
+  application_insights = {
+    name                  = join("-", ["appi", "appidemo", "psi", var.location])
+    workspace_resource_id = module.law.resource_id
+  }
 }
 
 # App Service for the Omega API
 module "app_omega" {
-    source  = "Azure/avm-res-web-site/azurerm"
-    version = "0.16.0"
+  source  = "Azure/avm-res-web-site/azurerm"
+  version = "0.16.0"
 
-    name                = join("-", ["app", "appidemo", "omega", var.location])
-    location            = var.location
-    resource_group_name = azurerm_resource_group.this.name
-    kind = "webapp"
+  name                = join("-", ["app", "appidemo", "omega", var.location])
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+  kind                = "webapp"
 
-    os_type                  = module.asp.resource.os_type
-    service_plan_resource_id = module.asp.resource_id
+  os_type                  = module.asp.resource.os_type
+  service_plan_resource_id = module.asp.resource_id
 
-    application_insights = {
-        name = join("-", ["appi", "appidemo", "omega", var.location])
-        workspace_resource_id = module.law.resource_id
-    }
+  application_insights = {
+    name                  = join("-", ["appi", "appidemo", "omega", var.location])
+    workspace_resource_id = module.law.resource_id
+  }
 }
