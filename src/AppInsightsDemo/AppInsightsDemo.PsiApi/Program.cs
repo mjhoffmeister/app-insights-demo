d0c5c99bbe96afa197aa1999357b5c0e1e260a07
Matt Hoffmeister <matt.hoffmeister@gmail.com>
2025-05-09 14:00:34 -0500

    Downgrade to .NET 8

--- src/AppInsightsDemo/AppInsightsDemo.PsiApi/Program.cs
+++ src/AppInsightsDemo/AppInsightsDemo.PsiApi/Program.cs
@@ -8,17 +8,7 @@ builder.Services.AddApplicationInsightsTelemetry();
 // Add HttpClient factory for making HTTP calls
 builder.Services.AddHttpClient();
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
